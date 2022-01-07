0db2ce4a06df69026648c927a2cfb2eaf3f63b73
Francis Bello <bellofrancis87@gmail.com>
2022-01-07 01:44:29 +0100

    "Tailored to Nigeria"

--- src/data.js
+++ src/data.js
@@ -1,32 +1,35 @@
 export default [
   {
-    title: "Mount Fuji",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    title: "Olumo Rock",
+    location: "Abeokuta",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Olumo+Rock,+Muhammadu+Jega+Rd,+110122/@7.2300078,3.4386739,15z/data=!4m5!3m4!1s0x0:0x310c8d61d35befdb!8m2!3d7.2300078!4d3.4386739",
     startDate: "12 Jan, 2021",
     endDate: "24 Jan, 2021",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    image: "/images/mt_fiji.png",
+      "It is located in the city of Abeokuta, Ogun State, and was normally used as a natural fortress during inter-tribal warfare in the 19th century. Its patron spirit is venerated in the Yoruba religion as an orisha.The name olumo is the combination of two words- 'olu' which means (God) and mo which means (moulded) the combination which is translated as God has ended our sufferings and trouble.",
+    image: "/images/olumo_rock.jpg",
   },
   {
-    title: "Sydney Opera House",
-    location: "Australia",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    title: "Ogbunike Cave",
+    location: "Ogbunike",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Ogbunike+Cave/@6.1858546,6.9060724,15z/data=!4m12!1m6!3m5!1s0x0:0xdb9a6f065658dee2!2sOgbunike+Cave!8m2!3d6.1858546!4d6.9060724!3m4!1s0x0:0xdb9a6f065658dee2!8m2!3d6.1858546!4d6.9060724?hl=en",
     startDate: "27 May, 2021",
     endDate: " 8 Jun, 2021",
     description:
-      "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
-    image: "/images/sydney.png",
+      "Located in a valley blanketed by tropical rain forest, the collection of caves has been in use over centuries by local people for whom it has particular spiritual significance. This spiritual significance is still apparent, as the 'Ime Ogba' celebration is undertaken every year to commemorate the discovery of the caves.",
+    image: "/images/ogbunike_cave.jpg",
   },
   {
-    title: "Geirangerfjord",
-    location: "Norway",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    title: "Erin-ijesa Waterfall",
+    location: "Erin-Ijesa",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Erin+Ijesha+Waterfall/@7.5651908,4.9021896,15z/data=!4m2!3m1!1s0x0:0x504dce56508e737b?sa=X&ved=2ahUKEwjS5b_Ns571AhVC1xoKHXyUBV8Q_BJ6BAguEAU",
     startDate: "01 Oct, 2021 ",
     endDate: "18 Nov, 2021",
     description:
-      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-    image: "/images/geiranger.png",
+      "It is a tourist attraction located in Oriade local government area, Osun State, Nigeria. The waterfalls was discovered in 1140 AD by one of the daughters of Oduduwa.",
+    image: "/images/erin-ijesa.jpg",
   },
 ];
